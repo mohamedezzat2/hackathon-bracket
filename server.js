f72378194a39ed7cd02ef8814750f3fd977bee96
Diaa Ahmed (IBS) <v-diaaahmed@microsoft.com>
2022-09-21 18:14:19 +0200

    add backend request

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const { BracketsManager } = require('brackets-manager');
 const { JsonDatabase } = require('brackets-json-db');
+const { async } = require('q');
 const storage = new JsonDatabase();
 const manager = new BracketsManager(storage);
 storage.reset();
@@ -11,6 +12,8 @@ const server = require('http').createServer(app);
 const todoData = {};
 const doneData = {};
 const doingData = {};
+
+const tournmentPlayers = {};
 
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
@@ -22,44 +25,78 @@ app.use(function (req, res, next) {
   next();
 })
 
+app.post("/addPlayer", async function(req, res) {
+  const id = req.body.id;
+  const players = req.body.players; 
+  if (tournmentPlayers[id] == undefined) {
+    tournmentPlayers[id] = []
+  }
+  tournmentPlayers[id].push(...players);
+  res.send(tournmentPlayers);
+});
+
+
+app.post("/create", async function(req, res) {
+  const id = parseInt(req.body.id);
+  const players = tournmentPlayers[id];
+  if (players === undefined){
+    res.status(406).send("players not found");
+
+    return 
+  }
+  await manager.create({
+    name: 'Example with BYEs',
+    tournamentId: id,
+    type: 'single_elimination',
+    seeding: players,
+    settings: {
+        seedOrdering: ['natural'],
+        balanceByes: false, // Default value.
+        size: players.length,
+    },
+  });
+
+  const data = await manager.get.tournamentData(id);
+  res.status(200).send(data);
+
+})
 
 app.get('/tournmentData', async function(req, res) {
-    storage.reset();
-    await manager.create({
-        name: 'Example with BYEs',
-        tournamentId: 0,
-        type: 'single_elimination',
-        seeding: [
-            'Fadi', 'Team 2',
-            'Team 3', 'Team 4',
-        ],
-        settings: {
-            seedOrdering: ['natural'],
-            balanceByes: false, // Default value.
-            size: 4,
-        },
-      });
-    await manager.update.match({
-        id: 0,
-        opponent1: { score: 10 , result:"win"},
-        opponent2: { score: 0 },
-      });
-
-      await manager.update.match({
-        id: 1,
-        opponent1: { score: 20, result:"win" },
-        opponent2: { score: 15 },
-      });
-
-      
-      await manager.update.match({
-        id: 3,
-        opponent1: { score: 23, result:"win" },
-        opponent2: { score: 11 },
-      });
-    const b = await manager.get.tournamentData(0);
-    res.status(200).send(b);
+    
+    const id = parseInt(req.query.id);
+    console.log(id);
+    const data = await manager.get.tournamentData(id);
+    res.status(200).send(data);
 });
+
+
+app.post('/update', async function(req, res) {
+    
+  const id = parseInt(req.body.id);
+  const match_id = parseInt(req.body.match_id);
+  const winner_index = parseInt(req.body.winner_index);
+
+  if (winner_index === 1) {
+    await manager.update.match({
+      id: match_id,
+      opponent1: { result:"win" },
+      opponent2: {  },
+    });
+  } else {
+    await manager.update.match({
+      id: match_id,
+      opponent1: {  },
+      opponent2: { result:"win" },
+    });
+
+  }
+ 
+  const data = await manager.get.tournamentData(id);
+  res.status(200).send(data);
+});
+
+
+
 
 server.listen(3000, function () {
   console.log('app listening on port 3000!');
